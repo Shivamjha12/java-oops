b17d603283699b68d03dde00e28099b301232231
Shivamjha12 <Jhasapna544@gmail.com>
2023-03-17 22:33:16 +0530

    17-03-2023 22-33pm

--- learnAbstraction/Shape.java
+++ learnAbstraction/Shape.java
@@ -1,4 +1,11 @@
 package learnAbstraction;
 
 public interface Shape {
+    int i=34; // It automatic add public static final in the starting of any data type in backend
+    void calculateArea(Double r);// It automatically make public abstarct method
+}
+class Circle implements Shape{
+    public void calculateArea(Double r){
+        System.out.println("The area of this circle is"+ Math.PI*r*r );
+    }
 }
